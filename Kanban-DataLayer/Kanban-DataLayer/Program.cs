28ae5e7deff517965d2a9693d4fd79112868f064
Jun Kang <jkang027@yahoo.com>
2016-02-01 12:40:34 -0800

    base project

---
+++ Kanban-DataLayer/Kanban-DataLayer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanban_DataLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new Entities())
+            {
+                foreach (var list in db.Lists)
+                {
+                    Console.WriteLine(list.Name);
+
+                    foreach (var card in list.Cards)
+                    {
+                        Console.WriteLine("\t" + card.Text);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
