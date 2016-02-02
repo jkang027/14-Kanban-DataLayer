7866ceb152e65df22ec33d7039cd5dca86228296
Jun Kang <jkang027@yahoo.com>
2016-02-01 17:02:16 -0800

    extras added

--- Kanban-DataLayer/Kanban-DataLayer/Program.cs
+++ Kanban-DataLayer/Kanban-DataLayer/Program.cs
@@ -8,21 +8,90 @@ namespace Kanban_DataLayer
 {
     class Program
     {
+        private static bool quit = false;
+        
         static void Main(string[] args)
         {
-            using (var db = new Entities())
+            while (quit == false)
             {
-                foreach (var list in db.Lists)
-                {
-                    Console.WriteLine(list.Name);
+                Console.WriteLine("What would you like to do? \"ADD CARD\", \"ADD LIST\", \"PRINT\" all, or \"QUIT\".");
+                string userinput = Console.ReadLine().ToUpper();
 
-                    foreach (var card in list.Cards)
+                using (var db = new Entities())
+                {
+                    switch (userinput)
                     {
-                        Console.WriteLine("\t" + card.Text);
-                    }
+                        case "PRINT":
+                            {
+                                foreach (var list in db.Lists)
+                                {
+                                    Console.WriteLine(list.Name);
+
+                                    foreach (var card in list.Cards)
+                                    {
+                                        Console.WriteLine("\t" + card.Text);
+                                    }
+                                    Console.WriteLine();
+                                }
+                            }
+                            break;
+
+                        case "ADD LIST":
+                            {
+                                Console.WriteLine("What would you like to name the list?");
+                                var newListName = Console.ReadLine();
+                                var newList = db.Set<List>();
+                                newList.Add(new List { Name = newListName, CreatedDate = DateTime.Now });
+                                db.SaveChanges();
+                                Console.WriteLine();
+                            }
+                            break;
+
+                        case "ADD CARD":
+                            {
+                                bool listName = true;
+                                int whichListId = 0;
+                                string whichListName = null;
+
+                                while (listName)
+                                {
+                                    Console.WriteLine("Which list would you like to add a new card to?");
+                                    whichListName = Console.ReadLine();
+                                    foreach (var list in db.Lists)
+                                    {
+                                        if (whichListName == list.Name)
+                                        {
+                                            whichListId = list.ListId;
+                                            listName = false;
+                                        }
+                                    }
+                                    if (listName)
+                                    {
+                                        Console.WriteLine("That is not an existing list.");
+                                    }
+                                }
+
+                                Console.WriteLine("What would you like the card text to be?");
+                                var newCardText = Console.ReadLine();
+                                db.Cards.Add(new Card { ListId = whichListId, CreatedDate = DateTime.Now, Text = newCardText });
+                                db.SaveChanges();
+                            }
+                            break;
+
+                        case "QUIT":
+                            {
+                                quit = true;
+                            }
+                            break;
+
+                        default:
+                            {
+                                Console.WriteLine("Please type \"ADD CARD\", \"ADD LIST\", \"PRINT\", or \"QUIT\".");
+                            }
+                            break;
+                    };
                 }
             }
-            Console.ReadLine();
         }
     }
 }
